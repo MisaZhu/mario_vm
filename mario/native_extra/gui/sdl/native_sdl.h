9aceecedf84db9abee407bde41a3ec2ca69887ad
May the source be with you <misa.zhu@gmail.com>
2018-09-09 02:28:16 +0800

    dir reorg

--- mario/native_extra/gui/sdl/native_sdl.h
+++
@@ -1,9 +0,0 @@
-#ifndef NATIVE_SDL_H
-#define NATIVE_SDL_H
-
-#include "mario_js.h"
-
-void reg_native_sdl(vm_t* vm);
-
-#endif
-
--- mario/test/fs.js
+++
@@ -1,9 +0,0 @@
-
-fd = FS.open("Makefile", FS.RDONLY);
-
-b = new Bytes(100);
-FS.read(fd, b, 100);
-
-Debug.dump(b.toString());
-
-FS.close(fd);
