c661e847d1f51813be99af5dccf1eea47ca67ec8
May the source be with you <misa.zhu@gmail.com>
2018-11-17 06:22:37 +0800

    throw error checking

--- vm/mario_vm.c
+++ vm/mario_vm.c
@@ -2188,7 +2188,10 @@ void vm_run(vm_t* vm) {
 			{
 				while(true) {
 					scope_t* sc = vm_get_scope(vm);
-					if(sc == NULL) break;
+					if(sc == NULL) {
+						_err("Error: 'throw' not in any try...catch!\n");
+						break;
+					}
 					if(sc->is_try) {
 						vm->pc = sc->pc;
 						break;
