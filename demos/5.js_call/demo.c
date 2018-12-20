3395fdac35169fc24e06e5b57af89ccfba66bacd
misazhu <misa.zhu@gmail.com>
2018-12-21 02:49:12 +0800

    demos

---
+++ demos/5.js_call/demo.c
@@ -0,0 +1,24 @@
+#include "mario_vm.h"
+
+const char* js = " \
+	function jsFunc() { \
+		return \"Hello JS world!\n\"; \
+	} \
+	";
+
+int main(int argc, char** argv) {
+	vm_t* vm = vm_new(compile);
+	vm_init(vm, NULL, NULL); //initialize the vm enviroment.
+
+	vm_load_run(vm, js); 
+
+	var_t* ret = call_m_func_by_name(vm, vm->root, "jsFunc", NULL);
+	if(ret != NULL) {
+		const char* s = var_get_str(ret);
+		_out_func(s);
+		var_unref(ret);
+	}
+	
+	vm_close(vm); //release
+	return 0;
+}
