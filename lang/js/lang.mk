043afa633d6f4135a0a5c7da415e20bba9335a46
misazhu <misa.zhu@gmail.com>
2025-09-05 17:33:36 +0800

    more native

--- lang/js/lang.mk
+++ lang/js/lang.mk
@@ -12,6 +12,7 @@ NATIVE_OBJS= $(NATIVE_PATH)/natives.o  \
 	$(NATIVE_PATH_BUILTIN)/console/native_console.o \
 	$(NATIVE_PATH_BUILTIN)/string/native_string.o \
 	$(NATIVE_PATH_BUILTIN)/array/native_array.o \
-	$(NATIVE_PATH_BUILTIN)/math/native_math.o \
-	$(NATIVE_PATH_BUILTIN)/json/native_json.o
-
+	$(NATIVE_PATH_BUILTIN)/json/native_json.o \
+	$(NATIVE_PATH_BUILTIN)/system/native_system.o \
+	$(NATIVE_PATH_BUILTIN)/fs/native_fs.o \
+	$(NATIVE_PATH_BUILTIN)/dir/native_dir.o
